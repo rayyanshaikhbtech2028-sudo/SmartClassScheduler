b08a96790badff9acab35605eadc64f60202ed92
rayyan <rayyan9136@gmail.com>
2026-04-15 16:34:29 +0530

    create admin via migration

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -2,22 +2,11 @@
 WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-from django.contrib.auth.models import User
-
-def create_admin():
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
-
-create_admin()
