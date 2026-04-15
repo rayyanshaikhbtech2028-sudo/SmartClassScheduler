6321f776a1c2be95d29c0d4cb57eb7a93c03140f
rayyan <rayyan9136@gmail.com>
2026-04-15 15:55:01 +0530

    fix admin creation

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
 
-
 class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        try:
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+        except:
+            pass
