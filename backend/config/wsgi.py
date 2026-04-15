a013fffb143271798d296841968e5859336d2d8e
rayyan <rayyan9136@gmail.com>
2026-04-15 16:22:12 +0530

    auto create admin

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+from django.contrib.auth.models import User
+
+def create_admin():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
+
+create_admin()
